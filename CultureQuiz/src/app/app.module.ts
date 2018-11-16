7141fe616db07d2f55ba05c3b153476a1b395116
KhanaeLeff <nicolas.nivlet@gmail.com>
2018-11-16 20:11:23 +0100

    add footer & contact component generate

--- CultureQuiz/src/app/app.module.ts
+++ CultureQuiz/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+
+// const appRoutes: Routes = [
+//   {
+//     path: 'contact',
+//     component: ContactComponent
+//   }
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule.forRoot(routes)
   ],
+  // exports: [
+  //   RouterModule
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
