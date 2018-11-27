d255793a0f1d76a850b06130d7bcd367f0dbb214
KhanaeLeff <nicolas.nivlet@gmail.com>
2018-11-27 17:53:38 +0100

    trying something, but wont work

--- CultureQuiz/src/app/app.module.ts
+++ CultureQuiz/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NgbdAccordionStatic } from './accordion-static';
+import { FormsModule, ReactiveFormsModule } from './@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MainComponent } from './main/main.component';
+import { AccordionStaticComponent } from './accordion-static/accordion-static.component';
 
 const appRoutes: Routes = [
   {
@@ -18,11 +24,17 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    NgbdAccordionStatic,
     ContactComponent,
-    MainComponent
+    MainComponent,
+    AccordionStaticComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
     RouterModule.forRoot(
       appRoutes,
       {enableTracing: true } // debugging purposes only
