8ede42211ef66d8be602b50581532a4b3b9368ba
KhanaeLeff <nicolas.nivlet@gmail.com>
2018-11-27 18:07:41 +0100

    add next to step 3 matrial angular modules

--- CultureQuiz/src/app/app-routing.module.ts
+++ CultureQuiz/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {MainComponent} from './main/main.component';
 import {ContactComponent} from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent },
   { path: 'contact', component: ContactComponent }
 ];
 @NgModule({
--- CultureQuiz/src/app/app.module.ts
+++ CultureQuiz/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NgbdAccordionStatic } from './accordion-static';
-import { FormsModule, ReactiveFormsModule } from './@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+// Import Material angular library //
+import { BrowserAnimationModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { ContactComponent } from './contact/contact.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { MainComponent } from './main/main.component';
-import { AccordionStaticComponent } from './accordion-static/accordion-static.component';
+import { AppRoutingModule } from './app-routing.module';
 
 const appRoutes: Routes = [
   {
@@ -24,16 +23,14 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    NgbdAccordionStatic,
     ContactComponent,
-    MainComponent,
-    AccordionStaticComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    BrowserAnimationModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
     NgbModule,
     RouterModule.forRoot(
       appRoutes,
